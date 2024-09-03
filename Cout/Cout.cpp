26552d38d7f6c57fb29af02fd551d4172d2c66ee
lazav <lazav@SHINDOUS11>
2024-09-03 21:09:32 +0300

    Add project files.

---
+++ Cout/Cout.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+int main()
+{
+    cout << "      \"To be" << endl;
+    cout << "              or not" << endl;
+    cout << "                      to be...\"''" << endl;
+    cout << "                              / Shakespeare /" << endl;
+
+    cout << "Press any key to continue to task 2: ";
+    Sleep(500);
+    cin.get();
+
+    cout << "I love C++!" << endl;
+    cout << "		I love C++!" << endl;
+    cout << "				I love C++!" << endl;
+    cout << "						I love C++!" << endl;
+    cout << "								I love C++!" << endl;
+
+    cout << "Press any key to continue to task 3: ";
+    Sleep(500);
+    cin.get();
+
+    cout << "September 2024:" << endl;
+    cout << "_____________________________" << endl;
+    cout << "| 26| 27| 28| 29| 30| 31|  1|" << endl;
+    cout << "_____________________________" << endl;
+    cout << "|  2|  3|  4|  5|  6|  7|  8|" << endl;
+    cout << "_____________________________" << endl;
+    cout << "|  9| 10| 11| 12| 13| 14| 15|" << endl;
+    cout << "_____________________________" << endl;
+    cout << "| 16| 17| 18| 19| 20| 21| 22|" << endl;
+    cout << "_____________________________" << endl;
+    cout << "| 23| 24| 25| 26| 27| 28| 29|" << endl;
+    cout << "_____________________________" << endl;
+    cout << "| 30|  1|  2|  3|  4|  5|  6|" << endl;
+    cout << "_____________________________" << endl;
+
+    cout << "Press any key to continue to task 4: ";
+    Sleep(500);
+    cin.get();
+
+    cout << "                        __---------------_" << endl;
+    cout << "                    __--__---------------=" << endl;
+    cout << "                __--__/               ||" << endl;
+    cout << "            __--__/                   ||" << endl;
+    cout << "        __--__/_______________________||" << endl;
+    cout << "    __--__/    ----------             ||" << endl;
+    cout << "__--||         | ,  ,  ,|             ||" << endl;
+    cout << "----||         |  ,  ,  |             I " << endl;
+    cout << "    ||         |,  ,  , |             I __________" << endl;
+    cout << "    ||         ----------             L |  |  |  |" << endl;
+    cout << "    ||                                I |  |  |  |" << endl;
+    cout << "--------------------------------------------------------" << endl;
+
+    cout << "Press any key to continue to task 5: ";
+    Sleep(500);
+    cin.get();
+
+    cout << "         Ivanov Vasyliy Ivanovich" << endl;
+    cout << "                       *" << endl;
+    cout << "                       *" << endl;
+    cout << "                       *" << endl;
+    cout << "                      * *" << endl;
+    cout << "                     *   *" << endl;
+    cout << "Ivanov Ivan Petrovich    Ivanova Galina Sergeevna" << endl;
+    cout << "        *                               *" << endl;
+    cout << "        *                               *" << endl;
+    cout << "        *                               *" << endl;
+    cout << "       * *                             * *" << endl;
+    cout << "      *   *                           *   *" << endl;
+    cout << "Ded Petya Babushka Lena       Ded Sergey  Babushka Ira" << endl;
+
+    cout << "Press any key to continue to task 6: ";
+    Sleep(500);
+    cin.get();
+
+    cout << "************    ************    ************    ********        ********        ****    ****" << endl;
+    cout << "************    ************    ************    ********       **********       ****    ****" << endl;
+    cout << "****            ****    ****    ****            **    **      ****    ****      ****    ****" << endl;
+    cout << "****            ****    ****    ****            **    **      ****    ****      ****    ****" << endl;
+    cout << "************    ****    ****    ****            **    **      ************      ************" << endl;
+    cout << "************    ****    ****    ****            **    **      ************      ************" << endl;
+    cout << "****    ****    ****    ****    ****          ************    ****    ****      ****    ****" << endl;
+    cout << "****    ****    ****    ****    ****          ************    ****    ****      ****    ****" << endl;
+    cout << "************    ************    ****          ****    ****    ****    ****      ****    ****" << endl;
+    cout << "************    ************    ****          ****    ****    ****    ****      ****    ****" << endl;
+
+
+    cout << "Press any key to continue to task 7: ";
+    Sleep(500);
+    cin.get();
+
+    cout << "*-----*" << endl;
+    cout << "       \\_"<< endl;
+    cout << "         \\_" << endl;
+    cout << "           *" << endl;
+    cout << "            \\_" << endl;
+    cout << "              \\_" << endl;
+    cout << "                *-----------*" << endl;
+    cout << "                |          /" << endl;
+    cout << "                |         /" << endl;
+    cout << "                *--------*" << endl;
+
+    cout << "Press any key to continue to task 8: ";
+    Sleep(500);
+    cin.get();
+
+    SetConsoleOutputCP(CP_UTF8);  // Set UTF-8 output
+    std::wcout << L"ПІБ:" << std::endl;
+    std::wcout << L"  Лазакович Богдан Іванович" << std::endl;
+    std::wcout << L"Дата народження:" << std::endl;
+    std::wcout << L"  12.12.2007" << std::endl;
+    std::wcout << L"Освіта:" << std::endl;
+    std::wcout << L"  Базова середня освіта" << std::endl;
+    std::wcout << L"  Молодша академія 'ШАГ'" << std::endl;
+    std::wcout << L"Робота:" << std::endl;
+    std::wcout << L"  Навчвння у Миколаївському політехнічному фаховому коледжі" << std::endl;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
